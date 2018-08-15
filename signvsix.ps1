702eddbdcfc8b0081a7bd84539c50cc80ae45ab0
leiguoqiang <leiguoqiang518@qq.com>
2018-08-15 17:02:10 +0800

    第一次提交

---
+++ signvsix.ps1
@@ -0,0 +1,12 @@
+param($PfxFilePath, $Password)
+
+$absolutePfxFilePath = Resolve-Path -Path $PfxFilePath
+Write-Output "Importing store certificate '$absolutePfxFilePath'..."
+
+Add-Type -AssemblyName System.Security
+$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+$cert.Import($absolutePfxFilePath, $Password, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]"PersistKeySet")
+$store = new-object system.security.cryptography.X509Certificates.X509Store -argumentlist "MY", CurrentUser
+$store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::"ReadWrite")
+$store.Add($cert)
+$store.Close()
